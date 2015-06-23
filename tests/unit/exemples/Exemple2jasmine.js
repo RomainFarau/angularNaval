43305299293c202e8bc87eae8d0b3b295fba9d23
Romain Farau <romain.farau@gmail.com>
2015-06-23 11:04:26 +0200

    karma la puissance

---
+++ tests/unit/exemples/Exemple2jasmine.js
@@ -0,0 +1,21 @@
+describe("Une suite avec 2 tests", function(){
+
+var count = 0;
+
+	beforeEach(function(){
+		this.valeur = 5;
+	});
+
+	afterEach(function(){
+		count++;
+	});
+
+	it("test 1", function(){
+		expect(this.valeur).toEqual(5);
+		expect(this.count).not.toBeDefined();
+	});
+
+	it("test 2", function(){
+		expect(count).toEqual(1);
+	});
+});
