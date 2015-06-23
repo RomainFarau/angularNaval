1cf60b235eb4896252f2698c3e0dae71176cbc4d
Romain Farau <romain.farau@gmail.com>
2015-06-23 11:19:27 +0200

    gitignore first

--- app/controllers/navalController.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-angular.module("navalModule")
-.controller('navalController', function(){
-	//var navalCtrl = this;
-})
---
+++ public/Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function(grunt){
+
+	
+
+	/*grunt.registerTask('hello','Hello Nantes',function(){
+		console.log('Hello Grunt, depuis Nantes!')
+	});
+
+	grunt.registerTask('etape1','etape 1 Visite Nantes',function(){
+		console.log('Etape 1 de la visite de Nantes!')
+	});
+	
+	grunt.registerTask('etape2','etape 2 Visite Nantes',function(){
+		console.log('Etape 2 de la visite de Nantes!')
+	});
+
+	grunt.registerTask('etape3','etape 3 Visite Nantes',function(){
+		console.log('Etape 3 de la visite de Nantes!')
+	});
+
+	grunt.registerTask('visite',['etape1','etape2','etape3']);
+
+	grunt.registerTask('default', 'hello');
+
+
+
+
+	grunt.initConfig({
+		ville:{
+			configNantes : 'Nantes',
+			configRennes : 'Rennes'
+		}
+	});
+
+	grunt.registerTask('nantes','ville:configNantes');
+	grunt.registerMultiTask('ville','Ville',function(){
+		console.log('Hello Grunt, depuis %s! avec la config %s', this.data, this.target);
+	});*/
+	grunt.initConfig({
+		copy: {
+			main:{
+				//src:['public/**/*.js','public/**/*.css','public/**/*.html'],
+				//dest:'tmp/',
+				files: {'tmp/' : ['public/**/*.js','public/**/*.css','public/**/*.html','public/index.html']},
+				expand: true
+			}
+		},
+		connect: {
+			dev:{
+				options:{
+					hostname:'localhost',
+					open :true,
+					base:'tmp/public',
+					port:3000,
+					livereload: true
+					
+				}
+			}
+		},
+		watch: {
+			dev:{
+				files: ['tmp/public/**/*.*','tmp/public/index.html'],
+				options: {
+					livereload: true
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('server',['connect','watch']);
+}
